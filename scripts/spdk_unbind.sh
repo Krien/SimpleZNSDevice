ee0dd2c5730eebf6e6f619eabde18848814c1b1e
Krijn Doekemeijer <krijn.doekemeijer@gmail.com>
2022-10-03 18:19:20 +0000

    Cleanup script directory

--- scripts/spdk_unbind.sh
+++ scripts/spdk_unbind.sh
@@ -1,5 +1,11 @@
-#!/usr/bin/env bash
-if [ -z ${SPDK_DIR}]
-    then bash ../dependencies/spdk/scripts/setup.sh reset
-    else bash ${SPDK_DIR}/scripts/setup.sh reset
+#!/bin/bash
+set -e
+
+DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+cd $DIR
+
+if [ -z ${SPDK_DIR}]; then 
+    bash ../dependencies/spdk/scripts/setup.sh reset
+else
+    bash ${SPDK_DIR}/scripts/setup.sh reset
 fi
