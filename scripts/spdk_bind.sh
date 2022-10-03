683a30da4c0577f7b0947aeb065ed01acb8772d2
Krijn Doekemeijer <krijn.doekemeijer@gmail.com>
2022-10-03 18:21:04 +0000

    Solve typo

--- scripts/spdk_bind.sh
+++ scripts/spdk_bind.sh
@@ -4,7 +4,7 @@ set -e
 DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 cd $DIR
 
-if [ -z ${SPDK_DIR}]; then 
+if [ -z ${SPDK_DIR} ]; then 
     bash ../dependencies/spdk/scripts/setup.sh
 else 
     bash ${SPDK_DIR}/scripts/setup.sh
--- scripts/spdk_unbind.sh
+++ scripts/spdk_unbind.sh
@@ -4,7 +4,7 @@ set -e
 DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 cd $DIR
 
-if [ -z ${SPDK_DIR}]; then 
+if [ -z ${SPDK_DIR} ]; then 
     bash ../dependencies/spdk/scripts/setup.sh reset
 else
     bash ${SPDK_DIR}/scripts/setup.sh reset
