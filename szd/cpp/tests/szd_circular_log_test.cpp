498a3c74a884fe31a7814a6a2da2e99f21c7d797
Krijn Doekemeijer <krijn.doekemeijer@gmail.com>
2022-11-08 21:56:51 +0000

    Test and improve szd_circular_logs

--- szd/cpp/tests/szd_circular_log_test.cpp
+++ szd/cpp/tests/szd_circular_log_test.cpp
@@ -12,122 +12,140 @@ namespace {
 
 class SZDTest : public ::testing::Test {};
 
-TEST_F(SZDTest, TestFillingACircularLogEphemerally) {
-  SZD::SZDDevice dev("TestFillingACircularLogEphemerally");
+static constexpr size_t needed_channels_for_circular_log = 3;
+static constexpr uint64_t begin_zone = 10;
+static constexpr uint64_t end_zone = 15;
+
+TEST_F(SZDTest, FillingACircularLogEphemerallyTest) {
+  SZD::SZDDevice dev("FillingACircularLogEphemerallyTest");
+  static constexpr uint64_t begin_zone_log = begin_zone + 1;
+  static constexpr uint64_t end_zone_log = end_zone - 2;
   SZD::DeviceInfo info;
-  SZDTestUtil::SZDSetupDevice(10, 15, &dev, &info);
-  SZD::SZDChannelFactory *factory =
-      new SZD::SZDChannelFactory(dev.GetDeviceManager(), 2);
-  SZD::SZDCircularLog log(factory, info, 11, 13, 1);
+  SZDTestUtil::SZDSetupDevice(begin_zone, end_zone, &dev, &info);
+  SZD::SZDChannelFactory *factory = new SZD::SZDChannelFactory(
+      dev.GetDeviceManager(), needed_channels_for_circular_log);
+  SZD::SZDCircularLog log(factory, info, begin_zone_log, end_zone_log, 1);
 
   // We need to reset all data if it is there, as always.
   ASSERT_EQ(log.ResetAll(), SZD::SZDStatus::Success);
   ASSERT_EQ(log.RecoverPointers(), SZD::SZDStatus::Success);
   // Test empty state
-  ASSERT_EQ(log.GetWriteHead(), 11 * info.zone_cap);
-  ASSERT_EQ(log.GetWriteTail(), 11 * info.zone_cap);
+  ASSERT_EQ(log.GetWriteHead(), begin_zone_log * info.zone_cap);
+  ASSERT_EQ(log.GetWriteTail(), begin_zone_log * info.zone_cap);
   ASSERT_TRUE(log.Empty());
-  uint64_t range = (13 - 11) * info.zone_cap * info.lba_size;
+  uint64_t range =
+      (end_zone_log - begin_zone_log) * info.zone_cap * info.lba_size;
   ASSERT_TRUE(log.SpaceLeft(range));
-  char buff[range];
-  memset(buff, 0, sizeof(buff));
+  ASSERT_EQ(log.SpaceAvailable(), range);
+
+  SZDTestUtil::RAIICharBuffer buff(range);
+  memset(buff.buff_, 0, sizeof(buff));
 
   // We can not read when nothing is written
-  uint64_t slba = 11 * info.zone_cap;
-  ASSERT_NE(log.Read(slba, buff, info.lba_size, true), SZD::SZDStatus::Success);
+  uint64_t slba = begin_zone_log * info.zone_cap;
+  ASSERT_NE(log.Read(slba, buff.buff_, info.lba_size, true),
+            SZD::SZDStatus::Success);
 
   // Write and read one entry
   uint64_t blocks;
   ASSERT_EQ(log.Append("TEST", sizeof("TEST"), &blocks, false),
             SZD::SZDStatus::Success);
   ASSERT_EQ(blocks, 1);
-  ASSERT_EQ(log.Read(slba, buff, info.lba_size, true), SZD::SZDStatus::Success);
-  ASSERT_TRUE(memcmp(buff, "TEST", sizeof("TEST")) == 0);
-  ASSERT_EQ(log.GetWriteHead(), 11 * info.zone_cap + 1);
-  ASSERT_EQ(log.GetWriteTail(), 11 * info.zone_cap);
+  ASSERT_EQ(log.Read(slba, buff.buff_, info.lba_size, true),
+            SZD::SZDStatus::Success);
+  ASSERT_TRUE(memcmp(buff.buff_, "TEST", sizeof("TEST")) == 0);
+  ASSERT_EQ(log.GetWriteHead(), begin_zone_log * info.zone_cap + 1);
+  ASSERT_EQ(log.GetWriteTail(), begin_zone_log * info.zone_cap);
   ASSERT_FALSE(log.Empty());
   ASSERT_TRUE(log.SpaceLeft(range - info.lba_size));
+  ASSERT_EQ(log.SpaceAvailable(), range - info.lba_size);
 
   // Fill entire device
-  char wbuff[range - info.lba_size];
-  SZDTestUtil::CreateCyclicPattern(wbuff, range - info.lba_size, info.lba_size);
-  ASSERT_EQ(log.Append(wbuff, range - info.lba_size, &blocks, true),
+  SZDTestUtil::RAIICharBuffer wbuff(range - info.lba_size);
+  SZDTestUtil::CreateCyclicPattern(wbuff.buff_, range - info.lba_size,
+                                   info.lba_size);
+  ASSERT_EQ(log.Append(wbuff.buff_, range - info.lba_size, &blocks, true),
             SZD::SZDStatus::Success);
   ASSERT_EQ(blocks, (range - info.lba_size) / info.lba_size);
-  ASSERT_EQ(log.Read(slba + 1, buff, range - info.lba_size, true),
+  ASSERT_EQ(log.Read(slba + 1, buff.buff_, range - info.lba_size, true),
             SZD::SZDStatus::Success);
-  ASSERT_TRUE(memcmp(buff, wbuff, range - info.lba_size) == 0);
-  ASSERT_EQ(log.GetWriteHead(), 13 * info.zone_cap);
-  ASSERT_EQ(log.GetWriteTail(), 11 * info.zone_cap);
+  ASSERT_TRUE(memcmp(buff.buff_, wbuff.buff_, range - info.lba_size) == 0);
+  ASSERT_EQ(log.GetWriteHead(), end_zone_log * info.zone_cap);
+  ASSERT_EQ(log.GetWriteTail(), begin_zone_log * info.zone_cap);
   ASSERT_FALSE(log.Empty());
   ASSERT_TRUE(log.SpaceLeft(0));
+  ASSERT_EQ(log.SpaceAvailable(), 0);
 
   // Try going out of bounds
   ASSERT_NE(log.Append("TEST", sizeof("TEST"), &blocks, false),
             SZD::SZDStatus::Success);
   ASSERT_EQ(blocks, 0);
-  ASSERT_EQ(log.GetWriteHead(), 13 * info.zone_cap);
+  ASSERT_EQ(log.GetWriteHead(), end_zone_log * info.zone_cap);
 
   // Does reset work?
   ASSERT_EQ(log.ResetAll(), SZD::SZDStatus::Success);
-  ASSERT_EQ(log.GetWriteHead(), 11 * info.zone_cap);
-  ASSERT_EQ(log.GetWriteTail(), 11 * info.zone_cap);
+  ASSERT_EQ(log.GetWriteHead(), begin_zone_log * info.zone_cap);
+  ASSERT_EQ(log.GetWriteTail(), begin_zone_log * info.zone_cap);
   ASSERT_TRUE(log.Empty());
   ASSERT_TRUE(log.SpaceLeft(range));
+  ASSERT_EQ(log.SpaceAvailable(), range);
 
   // Flood again
-  SZDTestUtil::CreateCyclicPattern(wbuff, range - info.lba_size, 0);
-  ASSERT_EQ(log.Append(wbuff, range - info.lba_size, &blocks, true),
+  SZDTestUtil::CreateCyclicPattern(wbuff.buff_, range - info.lba_size, 0);
+  ASSERT_EQ(log.Append(wbuff.buff_, range - info.lba_size, &blocks, true),
             SZD::SZDStatus::Success);
   ASSERT_EQ(blocks, (range - info.lba_size) / info.lba_size);
-  ASSERT_EQ(log.Read(slba, buff, range - info.lba_size, true),
+  ASSERT_EQ(log.Read(slba, buff.buff_, range - info.lba_size, true),
             SZD::SZDStatus::Success);
-  ASSERT_TRUE(memcmp(buff, wbuff, range - info.lba_size) == 0);
-  ASSERT_EQ(log.GetWriteHead(), 13 * info.zone_cap - 1);
-  ASSERT_EQ(log.GetWriteTail(), 11 * info.zone_cap);
+  ASSERT_TRUE(memcmp(buff.buff_, wbuff.buff_, range - info.lba_size) == 0);
+  ASSERT_EQ(log.GetWriteHead(), end_zone_log * info.zone_cap - 1);
+  ASSERT_EQ(log.GetWriteTail(), begin_zone_log * info.zone_cap);
   ASSERT_FALSE(log.Empty());
   ASSERT_TRUE(log.SpaceLeft(info.lba_size));
+  ASSERT_EQ(log.SpaceAvailable(), info.lba_size);
 }
 
 TEST_F(SZDTest, TestFillingACircularPersistently) {
   SZD::SZDDevice dev("TestFillingACircularPersistently");
   SZD::DeviceInfo info;
-  SZDTestUtil::SZDSetupDevice(10, 15, &dev, &info);
-  SZD::SZDChannelFactory *factory =
-      new SZD::SZDChannelFactory(dev.GetDeviceManager(), 2);
+  SZDTestUtil::SZDSetupDevice(begin_zone, end_zone, &dev, &info);
+  SZD::SZDChannelFactory *factory = new SZD::SZDChannelFactory(
+      dev.GetDeviceManager(), needed_channels_for_circular_log);
   factory->Ref();
 
   // Cleanup first round
   {
-    SZD::SZDCircularLog log(factory, info, 10, 15, 1);
+    SZD::SZDCircularLog log(factory, info, begin_zone, end_zone, 1);
 
     // We need to reset all data if it is there, as always.
     ASSERT_EQ(log.ResetAll(), SZD::SZDStatus::Success);
     ASSERT_EQ(log.RecoverPointers(), SZD::SZDStatus::Success);
-    ASSERT_EQ(log.GetWriteHead(), 10 * info.zone_cap);
-    ASSERT_EQ(log.GetWriteTail(), 10 * info.zone_cap);
+    ASSERT_EQ(log.GetWriteHead(), begin_zone * info.zone_cap);
+    ASSERT_EQ(log.GetWriteTail(), begin_zone * info.zone_cap);
   }
 
-  char buff[info.lba_size * 3];
-  SZDTestUtil::CreateCyclicPattern(buff, info.lba_size * 3, 0);
+  size_t range = info.lba_size * 3;
+  SZDTestUtil::RAIICharBuffer buff(range);
+  SZDTestUtil::CreateCyclicPattern(buff.buff_, range, 0);
 
   // We are going to repeatedly recreate logs, append some data and verify that
   // the pointers still match up after recovery.
-  for (uint64_t slba = 10 * info.zone_cap; slba < 15 * info.zone_cap - 3;
-       slba += 3) {
-    SZD::SZDCircularLog log(factory, info, 10, 15, 1);
+  for (uint64_t slba = begin_zone * info.zone_cap;
+       slba < end_zone * info.zone_cap - (range / info.lba_size);
+       slba += range / info.lba_size) {
+    SZD::SZDCircularLog log(factory, info, begin_zone, end_zone, 1);
     ASSERT_EQ(log.RecoverPointers(), SZD::SZDStatus::Success);
     ASSERT_EQ(log.GetWriteHead(), slba);
-    ASSERT_EQ(log.GetWriteTail(), 10 * info.zone_cap);
-    ASSERT_EQ(log.Append((const char *)buff, info.lba_size * 3, nullptr, true),
+    ASSERT_EQ(log.GetWriteTail(), begin_zone * info.zone_cap);
+    ASSERT_EQ(log.Append((const char *)buff.buff_, range, nullptr, true),
               SZD::SZDStatus::Success);
   }
 
   // last round, get and verify the data
-  SZD::SZDBuffer buffer((15 - 10) * info.zone_cap * info.lba_size,
+  SZD::SZDBuffer buffer((end_zone - begin_zone) * info.zone_cap * info.lba_size,
                         info.lba_size);
   {
-    SZD::SZDCircularLog log(factory, info, 10, 15, 1);
+    SZD::SZDCircularLog log(factory, info, begin_zone, end_zone, 1);
 
     ASSERT_EQ(log.RecoverPointers(), SZD::SZDStatus::Success);
     ASSERT_EQ(
@@ -139,7 +157,7 @@ TEST_F(SZDTest, TestFillingACircularPersistently) {
     ASSERT_EQ(buffer.GetBuffer((void **)&verify_buffer),
               SZD::SZDStatus::Success);
     for (uint64_t slba = 0; slba < 5 * info.zone_cap - 3; slba += 3) {
-      ASSERT_TRUE(memcmp(verify_buffer + slba * info.lba_size, buff,
+      ASSERT_TRUE(memcmp(verify_buffer + slba * info.lba_size, buff.buff_,
                          3 * info.lba_size) == 0);
     }
   }
@@ -147,42 +165,47 @@ TEST_F(SZDTest, TestFillingACircularPersistently) {
   factory->Unref();
 }
 
-TEST_F(SZDTest, TestCircularLogCircularPattern) {
-  SZD::SZDDevice dev("TestCircularLogCircularPattern");
+TEST_F(SZDTest, CircularLogCircularPatternTest) {
+  SZD::SZDDevice dev("CircularLogCircularPatternTest");
   SZD::DeviceInfo info;
-  SZDTestUtil::SZDSetupDevice(10, 15, &dev, &info);
-  SZD::SZDChannelFactory *factory =
-      new SZD::SZDChannelFactory(dev.GetDeviceManager(), 4);
-  SZD::SZDCircularLog log(factory, info, 10, 15, 1);
+  SZDTestUtil::SZDSetupDevice(begin_zone, end_zone, &dev, &info);
+  SZD::SZDChannelFactory *factory = new SZD::SZDChannelFactory(
+      dev.GetDeviceManager(), needed_channels_for_circular_log * 2);
+  SZD::SZDCircularLog log(factory, info, begin_zone, end_zone, 1);
 
   // We need to reset all data if it is there, as always.
   ASSERT_EQ(log.ResetAll(), SZD::SZDStatus::Success);
   ASSERT_EQ(log.RecoverPointers(), SZD::SZDStatus::Success);
 
   // Can not consume tail when empty
-  ASSERT_NE(log.ConsumeTail(10 * info.zone_cap, 10 * info.zone_cap + 3),
+  ASSERT_NE(log.ConsumeTail(begin_zone * info.zone_cap,
+                            begin_zone * info.zone_cap + 3),
             SZD::SZDStatus::Success);
 
   // Fill up to end - 3
-  uint64_t range =
-      (15 - 10) * info.zone_cap * info.lba_size - 3 * info.lba_size;
-  char buff[range];
-  SZDTestUtil::CreateCyclicPattern(buff, range, 0);
+  uint64_t range = (end_zone - begin_zone) * info.zone_cap * info.lba_size -
+                   3 * info.lba_size;
+  SZDTestUtil::RAIICharBuffer buff(range);
+  SZDTestUtil::CreateCyclicPattern(buff.buff_, range, 0);
   uint64_t blocks;
-  ASSERT_EQ(log.Append(buff, range, &blocks, true), SZD::SZDStatus::Success);
+  ASSERT_EQ(log.Append(buff.buff_, range, &blocks, true),
+            SZD::SZDStatus::Success);
 
   // Consume 3 entries from tail.
-  ASSERT_EQ(log.ConsumeTail(10 * info.zone_cap, 10 * info.zone_cap + 3),
+  ASSERT_EQ(log.ConsumeTail(begin_zone * info.zone_cap,
+                            begin_zone * info.zone_cap + 3),
             SZD::SZDStatus::Success);
-  ASSERT_NE(log.Append(buff, info.lba_size * 9, &blocks, true),
+  // We have no space left (even after consuming a bit)
+  ASSERT_NE(log.Append(buff.buff_, info.lba_size * 9, &blocks, true),
             SZD::SZDStatus::Success);
   // Can not eat same area again
-  ASSERT_NE(log.ConsumeTail(10 * info.zone_cap, 10 * info.zone_cap + 3),
+  ASSERT_NE(log.ConsumeTail(begin_zone * info.zone_cap,
+                            begin_zone * info.zone_cap + 3),
             SZD::SZDStatus::Success);
 
   // Try if pointers could be partially recovered on restart
   {
-    SZD::SZDCircularLog tlog(factory, info, 10, 15, 1);
+    SZD::SZDCircularLog tlog(factory, info, begin_zone, end_zone, 1);
     ASSERT_EQ(tlog.RecoverPointers(), SZD::SZDStatus::Success);
     ASSERT_EQ(tlog.GetWriteHead(), log.GetWriteHead());
     ASSERT_EQ(tlog.GetWriteTail(),
@@ -190,51 +213,93 @@ TEST_F(SZDTest, TestCircularLogCircularPattern) {
   }
 
   // Consume entire zone and try to append
-  ASSERT_EQ(log.ConsumeTail(10 * info.zone_cap + 3, 11 * info.zone_cap),
+  ASSERT_EQ(log.ConsumeTail(begin_zone * info.zone_cap + 3,
+                            (begin_zone + 1) * info.zone_cap),
             SZD::SZDStatus::Success);
-  ASSERT_EQ(log.Append(buff, info.lba_size * 9, &blocks, true),
+  ASSERT_EQ(log.Append(buff.buff_, info.lba_size * 9, &blocks, true),
             SZD::SZDStatus::Success);
 
   // Make some more space
-  ASSERT_EQ(log.ConsumeTail(11 * info.zone_cap, 12 * info.zone_cap),
+  ASSERT_EQ(log.ConsumeTail((begin_zone + 1) * info.zone_cap,
+                            (begin_zone + 2) * info.zone_cap),
             SZD::SZDStatus::Success);
 
-  char bufferr[6 * info.lba_size];
+  SZDTestUtil::RAIICharBuffer bufferr(6 * info.lba_size);
 
   // Repeatedly eat and append
   for (uint64_t slba = 0; slba < 5 * info.zone_cap - 6; slba += 6) {
-    uint64_t eat_address_first = 12 * info.zone_cap + slba;
-    uint64_t eat_address_second = 12 * info.zone_cap + slba + 6;
-    eat_address_second =
-        eat_address_first >= 15 * info.zone_cap
-            ? (eat_address_second - 15 * info.zone_cap) + 10 * info.zone_cap
-            : eat_address_second;
-    eat_address_first =
-        eat_address_first >= 15 * info.zone_cap
-            ? (eat_address_first - 15 * info.zone_cap) + 10 * info.zone_cap
-            : eat_address_first;
+    uint64_t eat_address_first = (begin_zone + 2) * info.zone_cap + slba;
+    uint64_t eat_address_second = (begin_zone + 2) * info.zone_cap + slba + 6;
+    eat_address_second = eat_address_first >= end_zone * info.zone_cap
+                             ? (eat_address_second - end_zone * info.zone_cap) +
+                                   begin_zone * info.zone_cap
+                             : eat_address_second;
+    eat_address_first = eat_address_first >= end_zone * info.zone_cap
+                            ? (eat_address_first - end_zone * info.zone_cap) +
+                                  begin_zone * info.zone_cap
+                            : eat_address_first;
 
     ASSERT_EQ(log.ConsumeTail(eat_address_first, eat_address_second),
               SZD::SZDStatus::Success);
     uint64_t waddress = log.GetWriteHead();
-    ASSERT_EQ(log.Append(buff, info.lba_size * 6, &blocks, true),
+    ASSERT_EQ(log.Append(buff.buff_, info.lba_size * 6, &blocks, true),
               SZD::SZDStatus::Success);
 
     // Test if the data is consistent
     {
-      ASSERT_EQ(log.Read(waddress, bufferr, 6 * info.lba_size, true),
+      ASSERT_EQ(log.Read(waddress, bufferr.buff_, 6 * info.lba_size, true),
                 SZD::SZDStatus::Success);
-      ASSERT_TRUE(memcmp(bufferr, buff, 6 * info.lba_size) == 0);
+      ASSERT_TRUE(memcmp(bufferr.buff_, buff.buff_, 6 * info.lba_size) == 0);
     }
 
     // Try if pointers could be partially recovered on restart
     {
-      SZD::SZDCircularLog tlog(factory, info, 10, 15, 1);
+      SZD::SZDCircularLog tlog(factory, info, begin_zone, end_zone, 1);
       ASSERT_EQ(tlog.RecoverPointers(), SZD::SZDStatus::Success);
       ASSERT_EQ(tlog.GetWriteHead(), log.GetWriteHead());
       ASSERT_EQ(tlog.GetWriteTail(),
                 (log.GetWriteTail() / info.zone_cap) * info.zone_cap);
     }
+  }
+}
+
+TEST_F(SZDTest, CircularLogMultipleReaderTest) {
+  SZD::SZDDevice dev("CircularLogCircularPatternTest");
+  SZD::DeviceInfo info;
+  SZDTestUtil::SZDSetupDevice(begin_zone, end_zone, &dev, &info);
+  SZD::SZDChannelFactory *factory = new SZD::SZDChannelFactory(
+      dev.GetDeviceManager(),
+      needed_channels_for_circular_log + /* 1 reader extra */ 1);
+  SZD::SZDCircularLog log(factory, info, begin_zone, end_zone, 2);
+  ASSERT_EQ(log.GetNumberOfReaders(), 2);
+
+  // Reset first
+  ASSERT_EQ(log.ResetAll(), SZD::SZDStatus::Success);
+  ASSERT_EQ(log.RecoverPointers(), SZD::SZDStatus::Success);
+
+  // Add some data
+  size_t range = info.lba_size * 3;
+  SZDTestUtil::RAIICharBuffer buffw(range);
+  SZDTestUtil::CreateCyclicPattern(buffw.buff_, range, 0);
+  ASSERT_EQ(log.Append(buffw.buff_, range, nullptr, true),
+            SZD::SZDStatus::Success);
+
+  // 2 readers
+  SZDTestUtil::RAIICharBuffer buffr1(range);
+  SZDTestUtil::RAIICharBuffer buffr2(range);
+  ASSERT_EQ(log.Read(begin_zone * info.zone_cap, buffr1.buff_,
+                     info.lba_size * 2, true, 0),
+            SZD::SZDStatus::Success);
+  ASSERT_EQ(log.Read(begin_zone * info.zone_cap, buffr2.buff_, info.lba_size,
+                     true, 1),
+            SZD::SZDStatus::Success);
+
+  // This should have been done with a memcmp in all fairness
+  for (size_t i = 0; i < info.lba_size * 2; i++) {
+    ASSERT_EQ(buffw.buff_[i], buffr1.buff_[i]);
+  }
+  for (size_t i = 0; i < info.lba_size; i++) {
+    ASSERT_EQ(buffw.buff_[i + info.lba_size * 2], buffr2.buff_[i]);
   }
 }
 
