bd36e2d99d0a553b3ee4e922c76ec3cde8c42f49
Krijn Doekemeijer <krijn.doekemeijer@gmail.com>
2022-11-09 21:25:54 +0000

    Solve DPDK linking issue

--- cmake/FindSPDK.cmake
+++ cmake/FindSPDK.cmake
@@ -19,16 +19,41 @@ pkg_search_module(DPDK REQUIRED IMPORTED_TARGET spdk_env_dpdk)
 pkg_search_module(SYS REQUIRED IMPORTED_TARGET spdk_syslibs)
 
 set(SPDK_INCLUDE_DIRS "${SPDK_INCLUDE_DIRS}")
-# use ";" otherwise the second and third argument become one?
-set(SPDK_LIB_DIRS "${SPDK_LIBRARY_DIRS};${DPDK_LIBRARY_DIRS};${SYS_STATIC_LIBRARY_DIRS}")
-set(SPDK_AND_DPDK_LIBRARIES "${SPDK_LIBRARIES}" "${DPDK_LIBRARIES}")
-list(REMOVE_DUPLICATES SPDK_AND_DPDK_LIBRARIES)
-set(SPDK_LIBRARY_DEPENDENCIES 
-    -Wl,--whole-archive -Wl,--as-needed
-    "${SPDK_AND_DPDK_LIBRARIES}"
-    -Wl,--no-whole-archive
-    "${SYS_STATIC_LIBRARIES}" 
-    -pthread
-)
+set(SPDK_LIBRARY_DIRS "${SPDK_LIBRARY_DIRS};${DPDK_LIBRARY_DIRS};${SYS_STATIC_LIBRARY_DIRS}")
 
-message("SPDK directory: ${SPDK_DIR}")
+# TODO: Solve this once there is one sane CMake for SPDK somewhere
+# The following is what is done in the wiki of SPDK, but does not work as of 2022-11-09.
+# It will use a shared lib, not what we want! It uses wrong files in both sections and wrong types (static vs linked).
+# set(SPDK_AND_DPDK_LIBRARIES "${SPDK_LINK_LIBRARIES}" "${DPDK_LINK_LIBRARIES}")
+# list(REMOVE_DUPLICATES SPDK_AND_DPDK_LIBRARIES)
+# set(SPDK_LIBRARY_DEPENDENCIES 
+#     -Wl,--whole-archive
+#     "${SPDK_AND_DPDK_LIBRARIES}"
+#     -Wl,--no-whole-archive
+#     "${SYS_STATIC_LIBRARIES}" 
+# )
+
+# Fix libs.
+#   issue 1: many libs erroneously point to .so, but must be .a
+#   issue 2: duplicates in DPDK and SPDK libs
+#   issue 3: spdk_env_dpdk needs to be whole-archive, unlike the other DPDK deps...
+#   issue 4: there is no SYS_STATIC_LINK_LIBRARIES, so we need a quick and dirty solution for now to get isa-l
+list(TRANSFORM SPDK_LINK_LIBRARIES REPLACE "[.]so$" ".a")
+list(TRANSFORM DPDK_LINK_LIBRARIES REPLACE "[.]so$" ".a")
+set(SPDK_ENV_DPDK_VAR "") 
+foreach(DPDK_LIB ${DPDK_LINK_LIBRARIES})
+    if(DPDK_LIB MATCHES ".*spdk_env_dpdk.*")
+        set(SPDK_ENV_DPDK_VAR "${DPDK_LIB}") 
+    endif()
+endforeach()
+list(APPEND SPDK_LINK_LIBRARIES "${SPDK_ENV_DPDK_VAR}")
+list(REMOVE_ITEM DPDK_LINK_LIBRARIES "${SPDK_ENV_DPDK_VAR}" ${SPDK_LINK_LIBRARIES})
+list(TRANSFORM SYS_STATIC_LIBRARIES REPLACE "isal" "${SYS_STATIC_LIBRARY_DIRS}/libisal.a")
+
+set(SPDK_LIBRARY_DEPENDENCIES 
+    -Wl,--whole-archive
+    "${SPDK_LINK_LIBRARIES}"
+    -Wl,--no-whole-archive
+    "${DPDK_LINK_LIBRARIES}" 
+    "${SYS_STATIC_LIBRARIES}" 
+)
