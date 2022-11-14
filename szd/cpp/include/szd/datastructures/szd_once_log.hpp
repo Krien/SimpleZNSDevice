fc4c3e287fd3ecee480a467544a6d504c50cb3e0
Krijn Doekemeijer <krijn.doekemeijer@gmail.com>
2022-11-14 14:32:58 +0000

    Update once_log API

--- szd/cpp/include/szd/datastructures/szd_once_log.hpp
+++ szd/cpp/include/szd/datastructures/szd_once_log.hpp
@@ -16,14 +16,18 @@
 #include <deque>
 #include <functional>
 #include <string>
+#include <variant>
 
 namespace SIMPLE_ZNS_DEVICE_NAMESPACE {
+
+typedef std::variant<uint32_t, SZDChannel *> queue_depth_or_external_channel;
+
 class SZDOnceLog : public SZDLog {
 public:
+  // Either specify an external channel (borrowed by once log), or a queue depth
   SZDOnceLog(SZDChannelFactory *channel_factory, const DeviceInfo &info,
              const uint64_t min_zone_nr, const uint64_t max_zone_nr,
-             const uint8_t number_of_writers,
-             SZDChannel **write_channel = nullptr);
+             const queue_depth_or_external_channel channel_definition);
   ~SZDOnceLog() override;
 
   // Direct IO
@@ -58,7 +62,7 @@ public:
   inline bool SpaceLeft(const size_t size,
                         bool alligned = true) const override {
     uint64_t alligned_size =
-        alligned ? size : write_channel_[0]->allign_size(size);
+        alligned ? size : write_channel_->allign_size(size);
     return alligned_size <= space_left_;
   }
 
@@ -67,60 +71,38 @@ public:
   inline uint8_t GetNumberOfReaders() const override { return 1; };
 
   inline uint64_t GetBytesWritten() const override {
-    uint64_t bytes_written = 0;
-    for (uint8_t i = 0; i < number_of_writers_; i++) {
-      bytes_written += write_channel_[i]->GetBytesWritten();
-    }
-    return bytes_written;
+    return write_channel_->GetBytesWritten();
   };
   inline uint64_t GetAppendOperationsCounter() const override {
-    uint64_t append_operations = 0;
-    for (uint8_t i = 0; i < number_of_writers_; i++) {
-      append_operations += write_channel_[i]->GetAppendOperationsCounter();
-    }
-    return append_operations;
+    return write_channel_->GetAppendOperationsCounter();
   };
   inline uint64_t GetBytesRead() const override {
-    return read_channel_->GetBytesRead();
+    return read_reset_channel_->GetBytesRead();
   };
   inline uint64_t GetReadOperationsCounter() const override {
-    return read_channel_->GetReadOperationsCounter();
+    return read_reset_channel_->GetReadOperationsCounter();
   };
   inline uint64_t GetZonesResetCounter() const override {
-    uint64_t reset_operations = 0;
-    reset_operations += read_channel_->GetZonesResetCounter();
-    return reset_operations;
+    return read_reset_channel_->GetZonesResetCounter();
   };
   inline std::vector<uint64_t> GetZonesReset() const override {
-    std::vector<uint64_t> resets = read_channel_->GetZonesReset();
-    return resets;
+    return read_reset_channel_->GetZonesReset();
   };
   inline std::vector<uint64_t> GetAppendOperations() const override {
-    std::vector<uint64_t> resets = write_channel_[0]->GetAppendOperations();
-    for (uint8_t i = 1; i < number_of_writers_; i++) {
-      std::vector<uint64_t> tmp = write_channel_[i]->GetAppendOperations();
-      std::transform(resets.begin(), resets.end(), tmp.begin(), tmp.begin(),
-                     std::plus<uint64_t>());
-    }
-    return resets;
+    return write_channel_->GetAppendOperations();
   };
 
 private:
   bool IsValidAddress(uint64_t lba, uint64_t lbas);
   // log
-  const uint8_t number_of_writers_;
   const uint64_t block_range_;
+  uint32_t max_write_depth_;
   uint64_t space_left_;
   uint64_t write_head_;
   uint64_t zasl_;
-  // stall writes
-  // SZDBuffer write_buffer_;
-  // references
-  // TODO: cleanup, we only use 1 channel at most, we have moved logic to queue
-  // pairs
-  SZDChannel **write_channel_;
-  uint32_t qpair_depth_;
-  SZDChannel *read_channel_;
+  // channels used
+  SZDChannel *write_channel_;
+  SZDChannel *read_reset_channel_;
   bool write_channels_owned_;
 };
 } // namespace SIMPLE_ZNS_DEVICE_NAMESPACE
